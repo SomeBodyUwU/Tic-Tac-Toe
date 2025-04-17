ed0a1f8cc6b87a1a70f8cd8f69c0567affc3f56c
SomeBodyUwU <dima@zosia.kiev.ua>
2025-04-17 16:10:29 +0300

    Handled logic for placing "O" and refactored code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import {useState} from 'react';
 
 
-function Square({value, onSquareClick}){
-    return <button className="square" onClick={onSquareClick}>{value}</button>;
+function Square({value, onSquareClick})
+{
+    return <button className="square" onClick={onSquareClick}>
+            {value}
+            </button>;
 }
 
-export default function Board(){
+export default function Board()
+{
+    const [xIsNext, setXIsNext] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
-    function handleClick(i){
+
+    function handleClick(i)
+    {
+        if(squares[i])
+        {
+            return;
+        }
+
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        xIsNext ? nextSquares[i] = "X" : nextSquares[i] = "O";
         setSquares(nextSquares);
+        setXIsNext(!xIsNext);
     }
     return (
         <>
